1ef355cb0963812148484cd7ab693c2b13b2cb6b
acoverstone <acoverstone@ufl.edu>
2017-03-26 14:50:26 -0400

    Added some blank classes

---
+++ src/com/iteration3/model/tile/DesertTerrain.java
@@ -0,0 +1,4 @@
+package com.iteration3.model.tile;
+
+public class DesertTerrain extends Terrain {
+}
---
+++ src/com/iteration3/model/tile/PastureTerrain.java
@@ -0,0 +1,4 @@
+package com.iteration3.model.tile;
+
+public class PastureTerrain extends Terrain {
+}
