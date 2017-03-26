fc05ae715d87f75e96639dcb0c21fbdb677772b8
acoverstone <acoverstone@ufl.edu>
2017-03-26 16:12:11 -0400

    Oops

--- src/com/iteration3/model/map/Map.java
+++ src/com/iteration3/model/map/Map.java
@@ -4,7 +4,26 @@ import com.iteration3.model.tile.River;
 import com.iteration3.model.tile.Tile;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Map {
+
+    private HashMap<Location, Tile> tiles;
+    private HashMap<Location, River> rivers;
+    // TODO: Add observers
+
+    public Map() {
+        tiles = new HashMap<Location, Tile>();
+        rivers = new HashMap<Location, River>();
+        // TODO: Initialize observers
+    }
+
+    public void addTileToMap(Location l, Tile t) {
+        tiles.put(l,t);
+    }
+
+    public void addRiverToMap(Location l, River r) {
+        rivers.put(l,r);
+    }
 
 }
