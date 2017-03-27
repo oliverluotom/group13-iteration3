ba207ffd70065350c84b353a624204f6b20a78af
Leslie Richardson <bookworm42795@gmail.com>
2017-03-27 18:05:19 -0400

    Created GameWindow with empty map and status view panels

--- src/com/iteration3/Main.java
+++ src/com/iteration3/Main.java
@@ -1,8 +1,25 @@
 package com.iteration3;
 
-public class Main {
+import com.iteration3.view.GameWindow;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GameWindow gameWindow = new GameWindow();
+
+        //TODO: Make size equal to screen resolution
+        Scene scene = new Scene(gameWindow, 1221, 726);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Map Editor");
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-
+        System.out.println("Map Editor has started");
+        launch(args);
     }
 }
--- src/test/MapTest.java
+++ src/test/MapTest.java
@@ -1,6 +1,11 @@
 package test;
 
 
+import com.iteration3.model.map.Map;
+import com.iteration3.model.map.MapFileManager;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Iterator;
 
 /**
@@ -9,20 +14,20 @@ import java.util.Iterator;
 public class MapTest {
 
     // TODO: Get relative path, was not working for me
-//    @Test
-//    public void testFileToMap() throws Exception{
-//        Map map = new Map();
-//        MapFileManager mapManager = new MapFileManager(map, "/Users/acoverstone/Desktop/Skool/COP4331/group13-iteration3/src/com/iteration3/model/map/mapFile.txt");
-//
-//        mapManager.fillMapFromTextFile();
-//
-//        assertEquals(map.getTiles().size(), 7);
-//        assertEquals(map.getRivers().size(), 2);
-//
-//        // need to print edges and tiles to check if they are correct
-//        map.printRivers();
-//        map.printTiles();
-//    }
+    @Test
+    public void testFileToMap() throws Exception{
+        Map map = new Map();
+        MapFileManager mapManager = new MapFileManager(map, "/Users/acoverstone/Desktop/Skool/COP4331/group13-iteration3/src/com/iteration3/model/map/mapFile.txt");
+
+        mapManager.fillMapFromTextFile();
+
+        assertEquals(map.getTiles().size(), 7);
+        assertEquals(map.getRivers().size(), 2);
+
+        // need to print edges and tiles to check if they are correct
+        map.printRivers();
+        map.printTiles();
+    }
 
 
 }
