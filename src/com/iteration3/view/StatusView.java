304f22dd86157476e0bf234227a9fda980c79639
Broseph00 <ricardio234@gmail.com>
2017-03-27 23:31:45 -0400

    Finished most of statusView, added a statusViewTest to display the pane

--- src/com/iteration3/view/StatusView.java
+++ src/com/iteration3/view/StatusView.java
@@ -1,4 +1,124 @@
 package com.iteration3.view;
 
-public class StatusView {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class StatusView extends VBox {
+	
+	Label terrainType, riverType, rotateOption;
+	Button submitButton, removeButton; 
+	Text selectTerrain, selectRiver;
+	
+	public StatusView(double width, double height) {
+		
+		super(10);
+		
+		this.setWidth(width);
+		this.setHeight(height);
+		
+		terrainType = new Label();
+		riverType = new Label();
+		rotateOption = new Label();
+		
+		submitButton = new Button("Create Tile");
+		removeButton = new Button("Remove Tile");
+		
+		selectTerrain = new Text("Select the tile's terrain");
+		selectRiver = new Text("Select the tile's river");
+		
+		initializeView();
+	}
+	
+	public StatusView() {
+		
+		super(10);
+		
+		terrainType = new Label();
+		riverType = new Label();
+		rotateOption = new Label();
+		
+		submitButton = new Button("Create Tile");
+		removeButton = new Button("Remove Tile");
+		
+		selectTerrain = new Text("Select the tile's terrain");
+	    selectRiver = new Text("Select the tile's river");
+		
+		initializeView();
+	}
+	
+	private void initializeView() {
+		
+		this.setAlignment(Pos.CENTER);
+		this.getChildren().addAll(selectTerrain,terrainType,selectRiver, riverType,rotateOption);
+		
+		HBox horizontalPane = new HBox(10);
+		horizontalPane.setAlignment(Pos.CENTER);
+		
+		horizontalPane.getChildren().addAll(submitButton,removeButton);
+		
+		this.getChildren().add(horizontalPane);
+	}
+	
+	protected void add(Node node) {
+		this.getChildren().add(node);
+	}
+	
+	public void highlightTerrainOption() {
+		selectTerrain.setFill(Color.BLUE);
+		selectRiver.setFill(Color.BLACK);
+		rotateOption.setTextFill(Color.BLACK);
+		
+	}
+	
+	public void highlightRiverOption() {
+		selectRiver.setFill(Color.BLUE);
+		selectTerrain.setFill(Color.BLACK);
+		rotateOption.setTextFill(Color.BLACK);
+	}
+	
+	public void highlightRotateOption() {
+		rotateOption.setTextFill(Color.BLUE);
+		selectTerrain.setFill(Color.BLACK);
+		selectRiver.setFill(Color.BLACK);
+	}
+	
+	public void invalidateTerrainOption() {
+		selectTerrain.setFill(Color.RED);
+		selectRiver.setFill(Color.BLACK);
+		rotateOption.setTextFill(Color.BLACK);
+	}
+	
+	public void invalidateRiverOption() {
+		selectRiver.setFill(Color.RED);
+		selectTerrain.setFill(Color.BLACK);
+		rotateOption.setTextFill(Color.BLACK);
+	}
+	
+	public void invalidateRotateOption() {
+		rotateOption.setTextFill(Color.RED);
+		selectTerrain.setFill(Color.BLACK);
+		selectRiver.setFill(Color.BLACK);
+	}
+	
+	public void setTerrainType(String terrain) {
+		terrainType.setText(terrain);
+	}
+	
+	public void setRiverType(String river) {
+		riverType.setText(river);
+	}
+	
+	public void setRotateOption(String rotation) {
+		rotateOption.setText(rotation);
+	}
 }
+
---
+++ src/test/StatusViewTest.java
@@ -0,0 +1,32 @@
+import com.iteration3.view.StatusView;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class StatusViewTest extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		StatusView view = new StatusView();
+		Scene scene =  new Scene(view,500,500);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Test StatusView");
+		primaryStage.show();
+		
+		view.setTerrainType("Pasture");
+		view.setRiverType("None");
+		view.setRotateOption("Rotate River Use left and right arrow keys");
+		
+		view.highlightTerrainOption();
+		
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+		
+	}
+}
