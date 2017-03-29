b93c7759df507c0751e70917cc86af6dc5bfadb4
David Weng <dkweng@gmail.com>
2017-03-28 21:31:59 -0400

    MapView showing tiles with offset

--- src/com/iteration3/view/MapView.java
+++ src/com/iteration3/view/MapView.java
@@ -60,8 +60,19 @@ public class MapView extends Pane{
         gc.setFill(Color.BLACK);
         gc.fillRect(0,0,getMapCanvas().getWidth(),getMapCanvas().getHeight());
         gc.setFill(Color.WHITE);
-        gc.drawImage(images.getImage("desert"), 200, 200);
+        //gc.drawImage(images.getImage("desert"), 200, 200);
         gc.drawImage(images.getImage("cursor"), 200, 200);
+        //gc.drawImage(images.getImage("desert"), 200, 200);
+        for(int x = -5; x <= 5; x++){
+            for(int y = -5; y <= 5; y++){
+                if(x%2==0){
+                    gc.drawImage(images.getImage("desert"), (x + 5) * 52, (y + 5) * 60 + 30);
+                }
+                else {
+                    gc.drawImage(images.getImage("desert"), (x + 5) * 52, (y + 5) * 60);
+                }
+            }
+        }
     }
 
     // Camera Navigation Methods
