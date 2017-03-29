666c5ab0861c1448bf8cb50800a4aecedd35190b
PGA <philipa@ufl.edu>
2017-03-29 02:05:49 -0400

    MapFileHandler and FileChooser implemented

--- src/com/iteration3/controller/MapFileController.java
+++ src/com/iteration3/controller/MapFileController.java
@@ -1,34 +1,44 @@
 package com.iteration3.controller;
 
+import com.iteration3.model.GameModel;
 import com.iteration3.model.map.*;
+import com.iteration3.view.GameWindow;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 import java.io.IOException;
 
 public class MapFileController {
-    Map map;
-    String fileDirectory;
-    String fileName;
-    MapFileManager fileManager;
+    private Map map;
+    private MapFileManager fileManager;
+    private EventHandler<ActionEvent> mapFileImporter, mapFileExporter;
+    private GameWindow gameWindow;
+    private GameModel gameModel;
 
-    MapFileController(Map map){
-        this.map = map;
+    private FileChooser fileChooser;
+    private String directoryPath;
+    private String fileName;
+
+    MapFileController(GameModel model, GameWindow window){
+        gameModel = model;
+        gameWindow = window;
         String fileLocation = "~/"; // TODO: default user home directory?
         String fileName = "boatsAndRoadsMap.txt";
+        createHandlers();
+    }
 
-        // TODO: verify that this is implemented as needed
-        EventHandler<ActionEvent> mapFileImporter = new EventHandler<ActionEvent>(){
-            @Override
-            public void handle(ActionEvent e){
-                // TODO
-            }
-        };
-        EventHandler<ActionEvent> mapFileExporter = new EventHandler<ActionEvent>(){
-            @Override
-            public void handle(ActionEvent e){
-                // TODO
-            }
-        };
+    private void setupFileChooser(){
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Select A Map File");
+        fileChooser.setSelectedExtensionFilter(new FileChooser.ExtensionFilter("Text File", "*.txt"));
+
+        directoryPath = "~/";
+        fileChooser.setInitialDirectory(new File(directoryPath));
+
+        fileName = "RoadsAndBoatsMap.txt";
+        fileChooser.setInitialFileName(fileName);
     }
 
     public void saveMap(String directory, String name){
@@ -39,11 +49,31 @@ public class MapFileController {
         }
     }
 
-    public void loadMap(String directory, String name) throws IOException {
+    public void loadMap(String directory, String name){
         try {
             fileManager.fillMapFromTextFile();
         } catch(IOException e){
             System.out.println("Error loading map!");
         }
+    }
+
+    private void createHandlers(){
+        mapFileImporter = new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+
+                loadMap(directoryPath, fileName);
+            }
+        };
+
+       mapFileExporter = new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent e){
+                saveMap(directoryPath, fileName);
+            }
+        };
+
+       gameWindow.setOnClickExport(mapFileExporter);
+       gameWindow.setOnClickImport(mapFileImporter);
     }
 }
