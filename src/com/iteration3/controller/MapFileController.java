3ba310fac9b6bc46ff8daa2665ca59d9a5bc6b40
PGA <philipa@ufl.edu>
2017-03-28 20:44:21 -0400

    Starting MapFIleController

--- src/com/iteration3/controller/MapFileController.java
+++ src/com/iteration3/controller/MapFileController.java
@@ -1,4 +1,18 @@
-package com.iteration3.controller;
+package iteration3.controller;
+
+import com.iteration3.model.map.*;
 
 public class MapFileController {
+    Map map;
+    String fileLocation;
+    String fileName;
+    MapFileManager fileManager;
+
+    MapFileController(Map map){
+        this.map = map;
+    }
+
+    saveMap(String directory, String name){
+
+    }
 }
