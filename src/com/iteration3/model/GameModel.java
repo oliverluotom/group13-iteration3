77f756bc8fb423b3c4f2cd934aa340c0084f3760
Leslie Richardson <bookworm42795@gmail.com>
2017-03-28 22:08:22 -0400

    moved map files over to game model class

--- src/com/iteration3/model/GameModel.java
+++ src/com/iteration3/model/GameModel.java
@@ -1,6 +1,9 @@
 package com.iteration3.model;
 
+import com.iteration3.model.map.Location;
 import com.iteration3.model.map.Map;
+import com.iteration3.model.tile.River;
+import com.iteration3.model.tile.Tile;
 
 public class GameModel {
     private Map map;
@@ -16,4 +19,21 @@ public class GameModel {
     public void setMap(Map map){
         this.map=map;
     }
+
+    public void addTileFromFile(Location l, Tile t) {
+        getMap().addTileFromFile(l,t);
+    }
+
+    public void addRiverFromFile(Location l, River r) {
+       getMap().addRiverFromFile(l,r);
+    }
+
+    public void addTileFromGUI(Location l, Tile t) {
+        getMap().addTileFromGUI(l,t);
+    }
+
+    public void addRiverFromGUI(Location l, River r) {
+        getMap().addRiverFromGUI(l,r);
+    }
+
 }
