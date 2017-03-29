a37b95abe2ac7e2199b64792470f921d42694251
PGA <philipa@ufl.edu>
2017-03-28 21:03:56 -0400

    todos added to gamewindow.java

--- src/com/iteration3/controller/MapFileController.java
+++ src/com/iteration3/controller/MapFileController.java
@@ -1,18 +1,29 @@
 package iteration3.controller;
 
 import com.iteration3.model.map.*;
+import javafx.event.EventHandler;
+
+import java.awt.event.ActionEvent;
 
 public class MapFileController {
     Map map;
-    String fileLocation;
+    String fileDirectory;
     String fileName;
     MapFileManager fileManager;
 
     MapFileController(Map map){
         this.map = map;
+        fileLocation = "default"; // TODO: default user home directory?
     }
 
     saveMap(String directory, String name){
 
     }
+
+    loadMap(String directory, String name){
+
+    }
+
+    EventHandler<ActionEvent> fileImportHandler = new EventHandler<ActionEvent>();
+           handle(ActionEvent e);
 }
